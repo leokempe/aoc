ccb59d1ef49c99a8d344134032784665cd1dd93f
Leo Kempe <leo.max.kempe@gmail.com>
2022-12-01 20:20:23 +0100

    Use Enum.max instead of own max function

--- lib/days/1/1.ex
+++ lib/days/1/1.ex
@@ -1,17 +1,9 @@
 defmodule Day1 do
-  defp max([n]), do: n
-  defp max([head | tail]) do
-    Enum.reduce([head | tail], fn 
-      n, acc when n >= acc -> n
-      n, acc when n < acc -> acc
-    end)
-  end
-
   def solve() do
     Aoc2022.input(1)
     |> Enum.chunk_by(&(&1 === ""))
     |> Enum.filter(&(&1 !== [""]))
     |> Enum.map(&(Enum.sum(Enum.map(&1, fn x -> String.to_integer(x) end))))
-    |> max
+    |> Enum.max
   end
 end

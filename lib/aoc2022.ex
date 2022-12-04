99ff4770564116b4106d168595dfd674cff4b590
Leo Kempe <leo.max.kempe@gmail.com>
2022-12-04 16:35:14 +0100

    Add function to get test input

--- lib/aoc2022.ex
+++ lib/aoc2022.ex
@@ -3,4 +3,9 @@ defmodule Aoc2022 do
     File.read!("lib/days/" <> Integer.to_string(date) <> "/input.txt")
     |> String.split(~r{(\r\n|\r)})
   end
+
+  def example_input(date) do
+    File.read!("lib/days/" <> Integer.to_string(date) <> "/example_input.txt")
+    |> String.split(~r{(\r\n|\r)})
+  end
 end

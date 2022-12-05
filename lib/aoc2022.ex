6f5b4840c033e76d1bc42d068a52b44604204c50
Leo Kempe <leo.max.kempe@gmail.com>
2022-12-05 14:32:43 +0100

    Fix regex in input parsing

--- lib/aoc2022.ex
+++ lib/aoc2022.ex
@@ -1,11 +1,11 @@
 defmodule Aoc2022 do
   def input(date) do
     File.read!("lib/days/" <> Integer.to_string(date) <> "/input.txt")
-    |> String.split(~r{(\r\n|\r)})
+    |> String.split(~r{(\r\n|\n)})
   end
 
   def example_input(date) do
     File.read!("lib/days/" <> Integer.to_string(date) <> "/example_input.txt")
-    |> String.split(~r{(\r\n|\r)})
+    |> String.split(~r{(\r\n|\n)})
   end
 end
